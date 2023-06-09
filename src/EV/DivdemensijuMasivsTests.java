653338bb8314e03924ef82de8a81a5adc602795e
E?V <liepa003@inbox.lv>
2023-06-10 00:09:18 +0300

    Programmas kļūdas labošana

--- src/EV/DivdemensijuMasivsTests.java
+++ src/EV/DivdemensijuMasivsTests.java
@@ -1,12 +1,10 @@
 package EV;
 
 import java.util.Arrays;
-
 import javax.swing.JOptionPane;
 
 public class DivdemensijuMasivsTests {
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         String[][] jautajumi = {
             {"Kā izveidot divdimensiju masīvu Java programmēšanas valodā?", "A) int[][] masivs = new int[5][5];", "B) int[] masivs = new int[5];", "C) int masivs = new int[5][5];", "D) int[] masivs = new int[][];"},
             {"Kā piekļūt divdimensiju masīva elementam ar indeksiem (rinda, kolonna)?", "A) masivs[rinda, kolonna];", "B) masivs[rinda][kolonna];", "C) masivs[rinda, kolonna];", "D) masivs(rinda)(kolonna);"},

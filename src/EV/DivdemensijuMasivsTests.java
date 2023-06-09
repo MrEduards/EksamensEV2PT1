26685a8344499ee657c6f896b00ce4fe7e8d0a1d
E?V <liepa003@inbox.lv>
2023-06-09 22:59:28 +0300

    Tiek pievienots datu tips(char), tajā tiek glabātas rakstzīmes, kas tiek lietotas, lai atzīmētu pareizās(Atbildes)

--- src/EV/DivdemensijuMasivsTests.java
+++ src/EV/DivdemensijuMasivsTests.java
@@ -3,7 +3,18 @@ package EV;
 public class DivdemensijuMasivsTests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[][] jautajumi = {
+	            {"Kā izveidot divdimensiju masīvu Java programmēšanas valodā?", "A) int[][] masivs = new int[5][5];", "B) int[] masivs = new int[5];", "C) int masivs = new int[5][5];", "D) int[] masivs = new int[][];"},
+	            {"Kā piekļūt divdimensiju masīva elementam ar indeksiem (rinda, kolonna)?", "A) masivs[rinda, kolonna];", "B) masivs[rinda][kolonna];", "C) masivs[rinda, kolonna];", "D) masivs(rinda)(kolonna);"},
+	            {"",},
+	            
+	        };
+		
+		char[][] pareizasAtbildes = {
+	            {'A', 'B'},
+	            {'B', 'C'},
+	            // Norādiet pareizās atbildes burtus katram jautājumam šeit
+	        };
 
 	}
 
